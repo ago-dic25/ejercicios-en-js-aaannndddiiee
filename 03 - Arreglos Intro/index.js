0504e0ff006acb5d4b8b5959fa4d178a6f53766a
Andrea Alfaro <150194253+aaannndddiiee@users.noreply.github.com>
2025-10-10 23:34:09 -0600

    estilos

--- 03 - Arreglos Intro/index.js
+++ 03 - Arreglos Intro/index.js
@@ -1,24 +1,40 @@
 const canciones = ["corporate-inspiring-motivational-healthcare-410903.mp3", "creepy-devil-dance-166764.mp3", "running-night-393139.mp3", "this-is-halloween-172354.mp3"];
 
 var cancionActual = new Array(1);
+var nombre = document.getElementById("n_song");
 
-var play = document.getElementById("play");
-const volumen = document.getElementById("volumen");
-var player = document.getElementById("player");
+const audio = document.getElementById("audio");
+const pausePlay = document.getElementById("play");
 
 function crearPlaylist(){
     const lista = document.createElement("ol");
     lista.setAttribute("id", "listaMusica");
-    for (var song in canciones){
+    for (let i = 0; i < canciones.length; i++){
         const item = document.createElement("li");
-        item.appendChild(document.createTexteNode(song));
-        item.setAttribute("id", canciones.indexOf(song));
+        item.textContent = canciones[i]; // más limpio que createTextNode
+        item.dataset.index = i;
+        lista.appendChild(item);
         lista.appendChild(item);
     }
     return lista;
 }
 
-document.getElementById("playlist").appendChild(crearPlaylist())
+function loadMusic(ruta){
+    var source = document.getElementById("source");
+    var folder = "musica";
+    source.src = folder + "/" + ruta;
+}
+
+document.getElementById("playlist").appendChild(crearPlaylist());
+
+pausePlay.addEventListener("click", function(){
+    if(audio.paused || audio.ended){
+        audio.play();
+    }else{
+        audio.pause();
+    }
+});
+
 var listado = document.getElementById("listaMusica");
 
 function reproduccionA(texto){
@@ -32,10 +48,16 @@ listado.addEventListener("click", function(e){
     player.play();
 });
 
-pausa.addEventListener("click", function(){
-    if(player.paused){
-        return player.play();
-    }else{
-        return player.pause();
-    }
+var listado = document.getElementById("listaMusica");
+
+function reproduccionA(texto){
+    document.getElementById("currentPlay").innerText = texto;
+}
+
+listado.addEventListener("click", function(e){
+    reproduccionA("Reproduciendo: " + itemClick.innerText);
+    loadMusic(itemClick.innerText);
+    cancionActual[0] = e.target.id;
+    player.play();
 });
+
